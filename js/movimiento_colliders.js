ff1e6b7a4955d7e0a5ed1d2d864b005721854937
JimmyPV <84383396+JimmyPV@users.noreply.github.com>
2021-06-12 22:02:50 +0200

    Fondo añadido y movimiento de este
    Ahora el fondo se mueve

--- js/movimiento_colliders.js
+++ js/movimiento_colliders.js
@@ -18,6 +18,9 @@ game.state.add('death', deathState);
 function preloadGame() {
     game.load.image('suelito', 'assets/imgs/suelo_arriba.png')
     game.load.spritesheet('frog', 'assets/imgs/FROGGO_caminando2.png', TAMAÑO_TILE, TAMAÑO_TILE, 6);
+    game.load.image('segunda_capa', 'assets/imgs/fondo3.png')
+    game.load.image('tercera_capa', 'assets/imgs/fondo2.png')
+    game.load.image('cuarta_capa', 'assets/imgs/fondo1.png')
 }
 
 
@@ -28,15 +31,18 @@ function createGame() {
 
     game.time.desiredFps = FPS_BUSCADOS;
 
-    //game.add.image(0, 0, 'background');
-    floor = game.add.tileSprite(0, 600, 10000, TAMAÑO_TILE, 'suelito');
+    fondo = game.add.tileSprite(0, 300, 10000, 900, 'cuarta_capa');
+    fondo2 = game.add.tileSprite(0, 500, 10000, 900, 'tercera_capa');
+    fondo3 = game.add.tileSprite(0, 650, 10000, 900, 'segunda_capa');
+
+    floor = game.add.tileSprite(0, 1100, 10000, TAMAÑO_TILE, 'suelito');
 
     //suelo
     game.world.setBounds(0, -100, 10000, 1124)
     //gravedad
     game.physics.arcade.gravity.y = FUERZA_GRAVEDAD;
     //añadir a personaje y sus fisicas
-    player = game.add.sprite(16, 16, 'frog');
+    player = game.add.sprite(16, 1000, 'frog');
     game.physics.arcade.enable([player, floor]);
 
     //collider suelo
@@ -62,6 +68,7 @@ function createGame() {
 function updateGame() {
 
     // game.physics.arcade.collide(player, layer);
+    
 
     player.body.velocity.x = 0;
     if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
@@ -69,12 +76,16 @@ function updateGame() {
         player.body.velocity.x = -150;
         player.animations.play('mLeft', 5, true);
         facing = 'mLeft';
+        fondo3.tilePosition.x += 0.5;
+        fondo2.tilePosition.x += 0.1;
     }
     else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
     {
         player.body.velocity.x = 150;
         player.animations.play('mRight', 5, true);
         facing = 'mRight';
+        fondo2.tilePosition.x -= 0.1;
+        fondo3.tilePosition.x -= 0.5;
     }
     
     if (game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR) && game.physics.arcade.collide(player, floor))
